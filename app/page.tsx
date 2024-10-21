3dcb176915036ad50beaebef83c7339413a65b28
devwithhammads <devwithhammad@gmail.com>
2024-10-21 04:45:58 -0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+const App = () => {
+        const groom = "hammad"
+        const bride = "hassan"
+        return (
+            <div className="w-full bg-blue-800 h-screen flex items-center justify-center">
+                <h1 className="text-white font-sans font-bold capitalize">{groom} weds {bride}</h1>
+            </div>
+        )
+    }
+
+export default App
+
